3993d1964bf42966e846a1da3d7ef2e5dc7b5ffe
JuanCarlosLopezHortelano <juancarloslhhellin@gmail.com>
2024-07-03 18:59:24 +0200

    Primer_componente_CSS_className

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,24 @@
+import './App.css'
+
 export function App () {
     return (
 
-        <article>
-        <header>
-            <img  alt="avatar de midulive" src="https://unavatar.io/baner.png" />
+        <article className='tw-followCard'>
+        <header className='tw-followCard-header'>
+            <img className='tw-followCard-avatar' alt="avatar de midulive" src="https://unavatar.io/baner.png" />
+            <div className='tw-followCard-info'>   <strong >JuanCarlos</strong>
+                    <span className='tw-followCard-infoUserName'>@jotasee</span>  
+            
+            </div>
         </header>
+
+        <aside>
+            <button className='tw-followCard-button'>
+                Seguir
+            </button>
+        </aside>
+
+
     </article>   
     )
        
