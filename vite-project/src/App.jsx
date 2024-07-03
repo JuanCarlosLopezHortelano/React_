5c249976d871f0333302dc20f05b707fb70a6031
JuanCarlosLopezHortelano <juancarloslhhellin@gmail.com>
2024-07-03 18:24:01 +0200

    Button

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+export function App () {
+    return (
+
+        <article>
+        <header>
+            <img  alt="avatar de midulive" src="https://unavatar.io/baner.png" />
+        </header>
+    </article>   
+    )
+       
+        
+    
+}
