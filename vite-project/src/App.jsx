2e04ca34fb8f46c65ea71cce9ba77178de4d453c
JuanCarlosLopezHortelano <juancarloslhhellin@gmail.com>
2024-07-04 11:02:48 +0200

    Finalizado

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,24 +1,45 @@
+import { Children } from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App () {
    const formatUserName = (userName) => `@${userName}`
-   const jotasee = { username: 'jotasee', formatUserName:formatUserName}
-   const ali = { userName: 'ali', formatUserName:formatUserName}
 
+   const users = [
+
+      {
+        userName: 'jotasee',
+        name_: 'JuanCarlos',
+        formatUserName:formatUserName,
+        isFollowing: true,
+        
+      },
+      { userName: 'ali', 
+         name_: 'Alicia',
+         formatUserName:formatUserName,
+         isFollowing: true,
+        
+      },
+    
+   ]
+
+
+  
    return(
       <section className='App'>
-      <TwitterFollowCard {...jotasee} initialIsFollowing={true}>
-         <h1>Juan Carlos</h1>
-         
-      </TwitterFollowCard> 
-
-      <TwitterFollowCard
-         formatUserName ={formatUserName}  
-         username="jotacee" >
-         <h1>Juan Carlos</h1>
-      </TwitterFollowCard> 
-      
+         {
+       users.map(({userName,formatUserName,isFollowing, name_}) => (
+         <TwitterFollowCard
+            username={userName}
+            name={name_}
+            initialIsFollowing={isFollowing}
+            formatUserName={formatUserName}
+          >
+          {name_}
+         </TwitterFollowCard>
+       ))
+       
+      }
       </section> 
    
 
