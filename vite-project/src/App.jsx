74d7aa97f1d07ae98e9d79305851963ec180884e
JuanCarlosLopezHortelano <juancarloslhhellin@gmail.com>
2024-07-04 09:31:31 +0200

    children --

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,20 @@ import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
 export function App () {
-   const formatUserName = (userName) => '@${userName}'
+   const formatUserName = (userName) => `@${userName}`
    return(
       <section className='App'>
-      <TwitterFollowCard formatUserName ={formatUserName} name="Juan Carlos" username="jotacee" isFollowing/>
-      <TwitterFollowCard formatUserName ={formatUserName} name="Juan Carlos" username="jotacee" isFollowing/>
+      <TwitterFollowCard
+         formatUserName ={formatUserName}  
+         username="jotacee" isFollowing>
+         <h1>Juan Carlos</h1>
+      </TwitterFollowCard> 
 
+      <TwitterFollowCard
+         formatUserName ={formatUserName}  
+         username="jotacee" isFollowing>
+         <h1>Juan Carlos</h1>
+      </TwitterFollowCard> 
       
       </section> 
    
--- vite-project/src/TwitterFollowCard.jsx
+++ vite-project/src/TwitterFollowCard.jsx
@@ -1,11 +1,11 @@
-export function TwitterFollowCard ({formatUserName, name, username, isFollowing}) {
+export function TwitterFollowCard ({children, formatUserName, username, isFollowing}) {
 
     const imgSrc = 'https://unavatar.io/${userName}';
     return (
         <article className='tw-followCard'>
         <header className='tw-followCard-header'>
             <img className='tw-followCard-avatar' alt="avatar de midulive" src={imgSrc}/>
-            <div className='tw-followCard-info'>   <strong >{name}</strong>
+            <div className='tw-followCard-info'>   <strong >{children}</strong>
                     <span className='tw-followCard-infoUserName'>{formatUserName(username)}</span>  
             
             </div>
